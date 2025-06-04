14fff6ddc485be7235aded3444179987725d0bd0
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-06-04 12:22:26 +0530

    backend-update

--- src/app/(auth)/callback/page.tsx
+++ src/app/(auth)/callback/page.tsx
@@ -1,19 +1,20 @@
-// import { onAuthenticateUser } from '@/actions/auth'
-// import { redirect } from 'next/navigation'
+import { onAuthenticateUser } from "@/actions/auth";
+import { redirect } from "next/navigation";
 
-// //TODO: Implement the auth callback page
-// export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
-// const AuthCallbackPage = async () => {
-//   const auth = await onAuthenticateUser()
+const AuthCallbackPage = async () => {
+  const auth = await onAuthenticateUser();
 
-//   if (auth.status === 200 || auth.status === 201) {
-//     redirect('/home')
-//   } else if(
-//     auth.status===403||auth.status===500||auth.status===400
-//   ){
-//     redirect('/')
-//   }
-// }
+  if (auth.status === 200 || auth.status === 201) {
+    redirect("/home");
+  } else if (
+    auth.status === 403 ||
+    auth.status === 500 ||
+    auth.status === 400
+  ) {
+    redirect("/");
+  }
+};
 
-// export default AuthCallbackPage
+export default AuthCallbackPage;
---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,28 @@
+import { onAuthenticateUser } from "@/actions/auth";
+import { Sidebar } from "@/components/ui/sidebar";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = async ({ children }: Props) => {
+  const userExist = await onAuthenticateUser();
+
+  if (!userExist.user) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <div className="flex w-full min-h-screen">
+      <Sidebar />
+      <div className="flex flex-col w-full h-screen overflow-auto px-4 scrollbar-hide container mx-auto">
+        {/* <Header user={userExist.user} /> */}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
